b4ca8ff2c1428b168c9bb5b896613c08211744b7
Thijs Jonkman <thijs.jonkman@virtualsciences.nl>
2014-01-25 22:19:54 +0100

    Hook up zest.releaser commands.

---
+++ gitflow/releaser/__init__.py
@@ -0,0 +1,7 @@
+from zest.releaser import fullrelease
+from zest.releaser import lasttagdiff
+from zest.releaser import lasttaglog
+from zest.releaser import longtest
+from zest.releaser import postrelease
+from zest.releaser import prerelease
+from zest.releaser import release
