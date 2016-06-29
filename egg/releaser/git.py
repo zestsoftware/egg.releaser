be1325993a9ab9df8109705565132517fe179862
Thijs Jonkman <thijs.jonkman@virtualsciences.nl>
2016-06-29 08:14:09 +0200

    Enhance original (OG) Git class instead of creating a new GitFlow

--- egg/releaser/git.py
+++ egg/releaser/git.py
@@ -2,20 +2,20 @@ import logging
 import ConfigParser
 import io
 
-from zest.releaser.git import Git
+from zest.releaser.git import Git as OGGit
+
+try:
+    from egg.releaser.utils import execute_command
+except ImportError:
+    # Old version?
+    from egg.releaser.utils import system as execute_command
 
 logger = logging.getLogger(__name__)
 
 
-class GitFlow(Git):
-    """ Add command proxy for GitFlow to already existing git commands.
+class Git(OGGit):
+    """ Command proxy for Git enhanced with gitflow commands.
     """
-
-    def cmd_create_tag(self, version, base=''):
-        msg = "Release-%s" % version
-        _start_cmd = 'git flow release start %s %s' % (version, base)
-        _finish_cmd = 'git flow release finish -m "%s" %s' % (msg, version)
-        return '; '.join([_start_cmd, _finish_cmd])
 
     def cmd_gitflow_release_start(self, version, base=''):
         return 'git flow release start %s %s' % (version, base)
@@ -43,3 +43,21 @@ class GitFlow(Git):
     def extensions(self):
         config = self._config()
         return ['gitflow'] if 'gitflow "branch"' in config.sections() else []
+
+    def cmd_create_tag(self, version, base=''):
+        if 'gitflow' in self.extensions:
+            msg = "Release-%s" % version
+            _start_cmd = 'git flow release start %s %s' % (version, base)
+            _finish_cmd = 'git flow release finish -m "%s" %s' % (msg, version)
+            return '; '.join([_start_cmd, _finish_cmd])
+        else:
+            super(OGGit, self).cmd_create_tag(self, version)
+
+    def gitflow_branches(self):
+        config = self._config()
+        return [
+            config.get('gitflow "branch"', branch)
+            for branch in ['develop', 'master']]
+
+    def current_branch(self):
+        return execute_command("git rev-parse --abbrev-ref HEAD").strip()
